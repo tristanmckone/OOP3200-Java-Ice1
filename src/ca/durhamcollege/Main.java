8caad0435b5bcc5a88b2127e723138ec95118ee7
Tristan Mckone <tristanmckone@gmail.com>
2021-11-01 23:04:02 -0400

    main created

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Person person = new Person("Tristan", 25);
+        person.SaysHello();
         System.out.println("Hello, World!");
     }
 }
--- src/ca/durhamcollege/Person.java
+++ src/ca/durhamcollege/Person.java
@@ -37,7 +37,7 @@ public class Person
 
 
     /**
-     *  Constructors
+     *  Default Constructor
      */
     public Person()
     {
